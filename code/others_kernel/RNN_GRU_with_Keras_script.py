347ac0327e181dfcd4f1f5dc8bc6547a3b102f81
8CF7K3FIUDL2BGT\Administrator <shohokuooo@gmail.com>
2018-01-09 18:12:45 +0800

    Add some comment of third RNN_GRU.

--- code/others_kernel/RNN_GRU_with_Keras_script.py
+++ code/others_kernel/RNN_GRU_with_Keras_script.py
@@ -20,8 +20,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-train = pd.read_csv('../../input/train.tsv', sep='\t', engine='python')
-test = pd.read_csv('../../input/test.tsv', sep='\t', engine='python')
+train = pd.read_csv('../../input/train.tsv', sep='\t', engine='python', nrows=10)
+test = pd.read_csv('../../input/test.tsv', sep='\t', engine='python', nrows=10)
 
 train['target'] = np.log1p(train['price'])
 
@@ -37,7 +37,7 @@ def simulate_test(test):
         return test_.copy()
     else:
         return test
-test = simulate_test(test)
+# test = simulate_test(test)
 print('new shape ', test.shape)
 print('[{}] Finished scaling test set...'.format(time.time() - start_time))
 
@@ -61,7 +61,7 @@ print('[{}] Finished handling missing data...'.format(time.time() - start_time))
 #PROCESS CATEGORICAL DATA
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
 print("Handling categorical variables...")
-le = LabelEncoder()  # 给字符串或者其他对象编码
+le = LabelEncoder()  # 给字符串或者其他对象编码, 从0开始编码
 
 le.fit(np.hstack([train.category_name, test.category_name]))
 train['category'] = le.transform(train.category_name)
@@ -73,18 +73,25 @@ test['brand'] = le.transform(test.brand_name)
 del le, train['brand_name'], test['brand_name']
 
 print('[{}] Finished PROCESSING CATEGORICAL DATA...'.format(time.time() - start_time))
-train.head(3)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', None, 'display.height', None):
+    print(train.head(3))
+    '''
+       train_id                                 name  item_condition_id                                      category_name  price  shipping                                   item_description    target  category  brand
+0         0  MLB Cincinnati Reds T Shirt Size XL                  3                                  Men/Tops/T-shirts   10.0         1                                 No description yet  2.397895         5      8
+1         1     Razer BlackWidow Chroma Keyboard                  3  Electronics/Computers & Tablets/Components & P...   52.0         0  This keyboard is in great condition and works ...  3.970292         3      4
+2         2                       AVA-VIV Blouse                  1                        Women/Tops & Blouses/Blouse   10.0         1  Adorable top with a hint of lace and a key hol...  2.397895        16      6
+    '''
 
 
 #PROCESS TEXT: RAW
 print("Text to seq process...")
 print("   Fitting tokenizer...")
 from keras.preprocessing.text import Tokenizer
-raw_text = np.hstack([train.category_name.str.lower(), 
-                      train.item_description.str.lower(), 
+raw_text = np.hstack([train.category_name.str.lower(),
+                      train.item_description.str.lower(),
                       train.name.str.lower()])
 
-tok_raw = Tokenizer()  # 分割文本成词，然后将词转成编码
+tok_raw = Tokenizer()  # 分割文本成词，然后将词转成编码(先分词，后编码)
 tok_raw.fit_on_texts(raw_text)
 print("   Transforming text to seq...")
 train["seq_category_name"] = tok_raw.texts_to_sequences(train.category_name.str.lower())
@@ -95,7 +102,13 @@ train["seq_name"] = tok_raw.texts_to_sequences(train.name.str.lower())
 test["seq_name"] = tok_raw.texts_to_sequences(test.name.str.lower())
 with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', None, 'display.height', None):
     print(train.head(3))
+'''
+   train_id                                 name  item_condition_id                                      category_name  price  shipping                                   item_description    target  category  brand     seq_category_name                               seq_item_description                           seq_name
+0         0  MLB Cincinnati Reds T Shirt Size XL                  3                                  Men/Tops/T-shirts   10.0         1                                 No description yet  2.397895         5      8      [55, 19, 20, 56]                                       [28, 69, 70]  [199, 200, 201, 20, 202, 11, 203]
+1         1     Razer BlackWidow Chroma Keyboard                  3  Electronics/Computers & Tablets/Components & P...   52.0         0  This keyboard is in great condition and works ...  3.970292         3      4  [57, 58, 59, 60, 61]  [71, 29, 30, 6, 72, 31, 1, 73, 74, 75, 76, 32,...                 [34, 204, 205, 29]
+2         2                       AVA-VIV Blouse                  1                        Women/Tops & Blouses/Blouse   10.0         1  Adorable top with a hint of lace and a key hol...  2.397895        16      6       [7, 19, 62, 21]  [89, 17, 9, 4, 90, 3, 91, 1, 4, 92, 93, 6, 2, ...                     [206, 207, 21]
 
+'''
 print('[{}] Finished PROCESSING TEXT DATA...'.format(time.time() - start_time))
 
 
@@ -108,18 +121,18 @@ print(dvalid.shape)
 
 #EMBEDDINGS MAX VALUE
 #Base on the histograms, we select the next lengths
-MAX_NAME_SEQ = 20 #17
-MAX_ITEM_DESC_SEQ = 60 #269
-MAX_CATEGORY_NAME_SEQ = 20 #8
+MAX_NAME_SEQ = 20 #17 name列转texts_to_sequences后的list的最大长度，不足会补足，过长会截断
+MAX_ITEM_DESC_SEQ = 60 #269, 同上，是item_description
+MAX_CATEGORY_NAME_SEQ = 20 #8, 同上，是category_name列的
 MAX_TEXT = np.max([np.max(train.seq_name.max())
                    , np.max(test.seq_name.max())
                    , np.max(train.seq_category_name.max())
                    , np.max(test.seq_category_name.max())
                    , np.max(train.seq_item_description.max())
                    , np.max(test.seq_item_description.max())])+2
-MAX_CATEGORY = np.max([train.category.max(), test.category.max()])+1
+MAX_CATEGORY = np.max([train.category.max(), test.category.max()])+1  # LE编码后最大值+1
 MAX_BRAND = np.max([train.brand.max(), test.brand.max()])+1
-MAX_CONDITION = np.max([train.item_condition_id.max(), 
+MAX_CONDITION = np.max([train.item_condition_id.max(),
                         test.item_condition_id.max()])+1
 
 print('[{}] Finished EMBEDDINGS MAX VALUE...'.format(time.time() - start_time))
@@ -142,7 +155,9 @@ def get_keras_data(dataset):
     }
     return X
 
-X_train = get_keras_data(dtrain)
+X_train = get_keras_data(dtrain)  # name:名字词编号pad列表, item_desc:描述词编号pad列表,
+                                  # brand:品牌编号, category:类别编号, category_name:类别词编号pad列表,
+                                  # item_condition: item_condition_id, num_vars: shipping
 X_valid = get_keras_data(dvalid)
 X_test = get_keras_data(test)
 
@@ -171,31 +186,31 @@ dr = 0.25
 def get_model():
     #params
     dr_r = dr
-    
+
     #Inputs
     name = Input(shape=[X_train["name"].shape[1]], name="name")
     item_desc = Input(shape=[X_train["item_desc"].shape[1]], name="item_desc")
     brand = Input(shape=[1], name="brand")
     category = Input(shape=[1], name="category")
-    category_name = Input(shape=[X_train["category_name"].shape[1]], 
+    category_name = Input(shape=[X_train["category_name"].shape[1]],
                           name="category_name")
     item_condition = Input(shape=[1], name="item_condition")
     num_vars = Input(shape=[X_train["num_vars"].shape[1]], name="num_vars")
-    
+
     #Embeddings layers
     emb_size = 60
-    
+
     emb_name = Embedding(MAX_TEXT, emb_size//3)(name)
     emb_item_desc = Embedding(MAX_TEXT, emb_size)(item_desc)
     emb_category_name = Embedding(MAX_TEXT, emb_size//3)(category_name)
     emb_brand = Embedding(MAX_BRAND, 10)(brand)
     emb_category = Embedding(MAX_CATEGORY, 10)(category)
     emb_item_condition = Embedding(MAX_CONDITION, 5)(item_condition)
-    
+
     rnn_layer1 = GRU(16) (emb_item_desc)
     rnn_layer2 = GRU(8) (emb_category_name)
     rnn_layer3 = GRU(8) (emb_name)
-    
+
     #main layer
     main_l = concatenate([
         Flatten() (emb_brand)
@@ -208,24 +223,24 @@ def get_model():
     ])
     main_l = Dropout(0.25)(Dense(128,activation='relu') (main_l))
     main_l = Dropout(0.1)(Dense(64,activation='relu') (main_l))
-    
+
     #output
     output = Dense(1,activation="linear") (main_l)
-    
+
     #model
     model = Model([name, item_desc, brand
                    , category, category_name
                    , item_condition, num_vars], output)
     #optimizer = optimizers.RMSprop()
     optimizer = optimizers.Adam()
-    model.compile(loss="mse", 
+    model.compile(loss="mse",
                   optimizer=optimizer)
     return model
 
 def eval_model(model):
     val_preds = model.predict(X_valid)
     val_preds = np.expm1(val_preds)
-    
+
     y_true = np.array(dvalid.price.values)
     y_pred = val_preds[:, 0]
     v_rmsle = rmsle(y_true, y_pred)
